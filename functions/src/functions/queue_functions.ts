e694d9d54834893bcb1a5688dd5794cad8862a22
xskullibur <simasas2222@gmail.com>
2020-07-06 12:04:37 +0800

    update to pop method

--- functions/src/functions/queue_functions.ts
+++ functions/src/functions/queue_functions.ts
@@ -2,6 +2,31 @@ import * as functions from "firebase-functions";
 
 import { QueueStatus, updateQueue } from './../utils/queue_utils';
 
+exports.popQueue = functions.https.onCall(async (data, context) => {
+
+    // Get Values
+    const queueId: string = data.queueId
+    const storeId: string = data.storeId
+    const queueService = new QueueService()
+        
+    //Guard against empty value
+    if(!queueId){
+        throw new functions.https.HttpsError("not-found" , "Could not find QueueID")
+    }
+    if(!storeId){
+        throw new functions.https.HttpsError("not-found" , "Could not find StoreID")
+    }
+
+    const currentQueue = queueService.popQueue(storeId)
+    const queueLength = queueService.getQueue(storeId).length
+
+    return {
+        currentQueueId: currentQueue!.queueId,
+        queueLength: queueLength
+    }
+
+})
+
 exports.queueListener = functions.https.onCall(async (data, context) => {
 
     // Get Values
