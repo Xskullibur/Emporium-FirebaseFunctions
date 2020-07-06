4cd42d733b34702183953cfcd64303f1e319fabd
xskullibur <simasas2222@gmail.com>
2020-07-06 12:45:06 +0800

    WIP

--- functions/src/functions/queue_functions.ts
+++ functions/src/functions/queue_functions.ts
@@ -27,54 +27,50 @@ exports.popQueue = functions.https.onCall(async (data, context) => {
 
 })
 
-exports.queueListener = functions.https.onCall(async (data, context) => {
+// exports.queueListener = functions.https.onCall(async (data, context) => {
 
-    // Get Values
-    const queueId: string = data.queueId
-    const storeId: string = data.storeId
+//     // Get Values
+//     const queueId: string = data.queueId
+//     const storeId: string = data.storeId
     
-    //Guard against empty value
-    if(!queueId){
-        throw new functions.https.HttpsError("not-found" , "Could not find QueueID")
-    }
-    if(!storeId){
-        throw new functions.https.HttpsError("not-found" , "Could not find StoreID")
-    }
+//     //Guard against empty value
+//     if(!queueId){
+//         throw new functions.https.HttpsError("not-found" , "Could not find QueueID")
+//     }
+//     if(!storeId){
+//         throw new functions.https.HttpsError("not-found" , "Could not find StoreID")
+//     }
 
-    functions.database.ref(`/emporium/globals/grocery_stores/${storeId}`)
-        .onUpdate((snapshot, context) => {
+//     functions.database.ref(`/emporium/globals/grocery_stores/${storeId}`)
+//         .onUpdate((snapshot, context) => {
 
-            const queueService = new QueueService()
+//             const queueService = new QueueService()
 
-            const data = snapshot.after.val()
-            const current_visitor_count = data['current_visitor_count']
-            const max_visitor_capacity = data['max_visitor_capacity']
+//             const data = snapshot.after.val()
+//             const current_visitor_count = data['current_visitor_count']
+//             const max_visitor_capacity = data['max_visitor_capacity']
 
-            if (current_visitor_count < max_visitor_capacity) {
-                const currentQueue = queueService.popQueue(storeId)
-                const queueLength = queueService.getQueue(storeId).length
-                return {
-                    currentQueueId: currentQueue!.queueId,
-                    queueLength: queueLength
-                }
-            }
-    })
-})
+//             if (current_visitor_count < max_visitor_capacity) {
+//                 const currentQueue = queueService.popQueue(storeId)
+//                 const queueLength = queueService.getQueue(storeId).length
+//                 return {
+//                     currentQueueId: currentQueue!.queueId,
+//                     queueLength: queueLength
+//                 }
+//             }
+//     })
+// })
 
 
 exports.joinQueue = functions.https.onCall(async (data, context) => {
 
     // Get Values
     const userId = context?.auth?.uid
-    const queueId: string = data.queueId
     const storeId: string = data.storeId
     
     //Guard against empty value
     if(!userId){
         throw new functions.https.HttpsError("not-found" , "Could not find UserID")
-    }
-    if(!queueId){
-        throw new functions.https.HttpsError("not-found" , "Could not find QueueID")
     }
     if(!storeId){
         throw new functions.https.HttpsError("not-found" , "Could not find StoreID")
